820b5137af5e6bdfb7c44fa9de282f3e45762b1b
Brandon-savage <lilsavageogjr@gmail.com>
2025-09-09 00:58:12 -0400

    Add files via upload

---
+++ assignment/02_06_famous_quote2.py
@@ -0,0 +1,3 @@
+famous_person = "Martin Luther king"
+message = f"{famous_person.title()} once said, “All men are created equil”"
+print(f"{famous_person.title()} once said, “All men are created equil”")
