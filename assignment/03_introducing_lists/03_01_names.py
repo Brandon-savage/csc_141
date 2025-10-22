00d37acfdae98e0934fd4758282cfad2bbd75bc2
Brandon-savage <lilsavageogjr@gmail.com>
2025-10-22 12:17:02 -0400

    Add files via upload

---
+++ assignment/03_introducing_lists/03_01_names.py
@@ -0,0 +1,4 @@
+
+names = ["Brandon Savage", "Autumn Smith", "Brian Dawkins" ]
+print(names[0])
+print(names[1])
---
+++ assignment/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,6 @@
+cars = ['Ford', 'Bmw','Pagani']
+print(cars)
+
+
+cars[0] = 'gt40'
+print(cars)
