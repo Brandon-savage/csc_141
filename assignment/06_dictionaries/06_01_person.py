864f897ce60b4dde6af181b0d149cab5c9a86089
Brandon-savage <lilsavageogjr@gmail.com>
2025-10-22 17:37:10 -0400

    Add files via upload

---
+++ assignment/06_dictionaries/06_01_person.py
@@ -0,0 +1,11 @@
+person = {
+    "first_name": "brandon",
+    "last_name": "savage",
+    "age": 19,
+    "city": "baltimore"
+}
+
+print("First name:", person["first_name"])
+print("Last name:", person["last_name"])
+print("Age:", person["age"])
+print("City:", person["city"])
