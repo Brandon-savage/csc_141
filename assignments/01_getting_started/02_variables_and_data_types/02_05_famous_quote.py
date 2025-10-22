e3c3c915aa85ec109cb314c0817807bcbdbef1a9
Brandon-savage <lilsavageogjr@gmail.com>
2025-10-22 17:50:19 -0400

    Add files via upload

---
+++ assignments/01_getting_started/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,2 @@
+message = " once said, “All men are created equil”"
+once said, “All men are created equil”"print("Martin luther king )
---
+++ assignments/01_getting_started/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,14 @@
+"this program file shows what my favorite number is."
+
+
+message = f"{favorite_number.title()} is my favorite number"
+print(message)
+
+
+
+
+"This program shows an insperational qoute from MLK"
+
+famous_person = "Martin Luther king"
+message = f"{famous_person.title()} once said, “All men are created equil”"
+print(f"{famous_person.title()} once said, “All men are created equil”")
